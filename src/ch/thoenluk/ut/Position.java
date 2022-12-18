feb09c0b395a36d5a5ff336842bac138a76bae01
thoenluk <Lukas.thoeni@gmx.ch>
2022-12-18 11:11:43 +0100

    Day 14: Solve day 14
    Question for a moment what was the point of simulating all this sand when not getting trapped by it still involves running to the edge as fast as possible.
    
    Carefully brush sand off of medicinal weeds stash.
    
    Attempt to educate elves telepathically on how they can all rot and I'm going to have an adventure on my own.

--- src/ch/thoenluk/ut/Position.java
+++ src/ch/thoenluk/ut/Position.java
@@ -5,6 +5,16 @@ import java.util.List;
 
 public record Position(int y, int x) {
 
+    //---- Static Methods
+
+    public static Position fromString(String description) {
+        final String[] coordinates = UtStrings.splitCommaSeparatedString(description);
+        return new Position(UtParsing.cachedParseInt(coordinates[1]), UtParsing.cachedParseInt(coordinates[0]));
+    }
+
+
+    //---- Methods
+
     public int getDistanceFrom(Position other) {
         return Math.abs(x() - other.x())
                 + Math.abs(y() - other.y());
@@ -28,6 +38,35 @@ public record Position(int y, int x) {
     public Position offsetBy(Position offset) {
         return new Position(this.y + offset.y(), this.x + offset.x());
     }
+
+    public List<Position> getPathTo(Position other) {
+        final List<Position> path = new LinkedList<>();
+        final Position offset = new Position(other.y() - y(), other.x() - x());
+
+        if (
+                !(
+                    offset.y() == 0
+                    || offset.x() == 0
+                    || Math.abs(offset.y()) == Math.abs(offset.x())
+                )
+        ) {
+            throw new IllegalArgumentException(String.format("Requested path between %s and %s would require sub-Position precision!", this, other));
+        }
+
+        final Position direction = new Position(
+                (int) Math.signum(offset.y()),
+                (int) Math.signum(offset.x())
+        );
+
+        for (int i = 0; i <= getRoleplayingDistanceFrom(other); i++) {
+            path.add(this.offsetBy(new Position(direction.y() * i, direction.x() * i)));
+        }
+
+        return path;
+    }
+
+
+    //---- Inner enum
 
     public enum NeighbourDirection {
         CARDINAL(List.of(new Position(-1, 0), new Position(0, -1), new Position(0, 1), new Position(1, 0))),
